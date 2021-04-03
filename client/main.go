596148c6c989147d751bfacf4b017d3054c4304f
ZYK <15986875679@163.com>
2021-04-03 17:44:34 +0800

    feat: add 选择菜单

--- client/main.go
+++ client/main.go
@@ -11,6 +11,7 @@ type Client struct {
 	ServerPort int
 	Name       string
 	Conn       net.Conn
+	flag       int // 当前用户选择模式
 }
 
 func NewClient(serverIp string, serverPort int) *Client {
@@ -18,6 +19,7 @@ func NewClient(serverIp string, serverPort int) *Client {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 	// 客户端发送请求给服务端要求建立链接
 	conn, error := net.Dial("tcp", fmt.Sprintf("%s:%d", client.ServerIp, client.ServerPort))
@@ -41,6 +43,50 @@ func init() {
 	flag.IntVar(&serverPort, "port", 8888, "默认端口")
 }
 
+func Menu(client *Client) bool {
+	var flag int
+	fmt.Println("1. 公共聊天模式")
+	fmt.Println("2. 私人聊天模式")
+	fmt.Println("3. 重命名")
+	fmt.Println("0. 退出")
+
+	fmt.Scanln(&flag)
+
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		fmt.Println("你选择了:", flag)
+		return true
+	} else {
+		fmt.Println("输入合法的选项")
+		return false
+	}
+}
+
+func (c *Client) Run() {
+	for {
+		if c.flag != 0 {
+			for {
+				isChoose := Menu(c)
+				if isChoose {
+					break
+				}
+			}
+			// 根据不同的选择模式去处理
+			// switch 默认情况下 case 最后自带 break 语句，匹配成功后就不会执行其他 case，如果我们需要执行后面的 case，可以使用 fallthrough
+			switch c.flag {
+			case 1:
+				fmt.Println(">>>>>>>> choose 公聊模式 <<<<<<<<")
+			case 2:
+				fmt.Println(">>>>>>>> choose 私聊模式 <<<<<<<<")
+			case 3:
+				fmt.Println(">>>>>>>> choose 重新命名 <<<<<<<<")
+			}
+		} else {
+			break
+		}
+	}
+}
+
 func main() {
 	// 命令行解析
 	flag.Parse()
@@ -51,6 +97,5 @@ func main() {
 		return
 	}
 	fmt.Println("NewClient success")
-
-	select {}
+	client.Run()
 }
